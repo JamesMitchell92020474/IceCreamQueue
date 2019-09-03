7d155fc377b7dd4f2957aa2186cc1babbd59c8e9
James Mitchell <jamesmitchelldesign1@gmail.com>
2019-09-03 16:13:51 +1200

    Initial Commit

---
+++ src/icecreamqueue/IceCreamQueue.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package icecreamqueue;
+
+/**
+ *
+ * @title IceCreamQueue.java
+ * @author James Mitchell 92020474
+ * @date 3/09/19
+ * 
+ */
+public class IceCreamQueue {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
